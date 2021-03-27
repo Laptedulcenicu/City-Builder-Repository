375a9b8b1fe19f2b39bad208fa3c39e01ac01aa4
Nicu <43432500+Laptedulcenicu@users.noreply.github.com>
2021-03-27 23:14:23 +0200

    changed texture size

---
+++ CityBuilder/Assets/_CityBuilder/DeleteAllColliders.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace _CityBuilder
+{
+    public class DeleteAllColliders : MonoBehaviour
+    {
+        [ContextMenu("Do Something")]
+        void Do()
+        {
+            List<GameObject>  allGameObject = GameObject.FindObjectsOfType<GameObject>().ToList();
+
+            foreach (var o in allGameObject)
+            {
+
+             Collider currentCollider = o.GetComponent<Collider>();
+             if (currentCollider)
+             {
+                 DestroyImmediate(currentCollider);
+             }
+            }
+            
+            
+        }
+
+    }
+}
