938cab9df754ac87efeeb89ddcf63fdf7c7c0552
Nicu <nicusor.laptedulce@gmail.com>
2021-04-26 23:45:25 +0300

    fix Repair

--- CityBuilder/Assets/_CityBuilder/Scripts/UI/PopUpStructurePanel/RepairStructurePopUp.cs
+++ CityBuilder/Assets/_CityBuilder/Scripts/UI/PopUpStructurePanel/RepairStructurePopUp.cs
@@ -1,13 +1,14 @@
 ﻿using System.Collections.Generic;
 using _CityBuilder.Scripts.Global_Manager;
 using _CityBuilder.Scripts.Scriptable_Object.Containers;
+using _CityBuilder.Scripts.StructureModel;
 using _CityBuilder.Scripts.Test_Script;
 
 namespace _CityBuilder.Scripts.UI.PopUpStructurePanel
 {
     public class RepairStructurePopUp : InfoStructurePopUp
     {
-        private List<NecessaryResourcesData> repairResourceData;
+        private List<NecessaryResourcesData> repairResourceData = new List<NecessaryResourcesData>();
 
         protected override void ConfirmButton()
         {
@@ -15,19 +16,25 @@ namespace _CityBuilder.Scripts.UI.PopUpStructurePanel
             {
                 GameResourcesManager.AddResourceAmount(necessaryResourcesData.Resource, necessaryResourcesData.Amount);
             }
+
+            LifeStatusData lifeStatusData = infoBuildingPanel.CurrentStructure.Configuration.StatusData;
+            lifeStatusData.Initialize(lifeStatusData.MAXHealth, lifeStatusData.MAXHealth);
+
+            CancelButton();
+            infoBuildingPanel.CancelButton();
         }
 
         public override void Show()
         {
             base.Show();
-            repairResourceData.Clear();
+
 
             int currentLifePercent =
                 (int) (infoBuildingPanel.CurrentStructure.Configuration.StatusData.MAXHealth *
                        infoBuildingPanel.PercentOffsetRepairPrice) / infoBuildingPanel.CurrentStructure.Configuration
                     .StatusData.CurrentHealth;
 
-            repairResourceData = new List<NecessaryResourcesData>();
+            repairResourceData.Clear();
 
             foreach (NecessaryResourcesData necessaryResourcesData in infoBuildingPanel.CurrentShopItem
                 .NecessaryResourcesDataList)
@@ -41,7 +48,7 @@ namespace _CityBuilder.Scripts.UI.PopUpStructurePanel
             foreach (NecessaryResourcesData necessaryResourcesData in repairResourceData)
             {
                 MoneyTextData moneyTextData = moneyList.Find(e => e.resourceType == necessaryResourcesData.Resource);
-                print(  moneyTextData.moneyAmount.transform.parent.gameObject.name);
+                print(moneyTextData.moneyAmount.transform.parent.gameObject.name);
                 moneyTextData.moneyAmount.transform.parent.gameObject.SetActive(true);
                 moneyTextData.moneyAmount.color = necessaryResourcesData.Amount < 0
                     ? infoStructurePopUpList.LoseMoney
