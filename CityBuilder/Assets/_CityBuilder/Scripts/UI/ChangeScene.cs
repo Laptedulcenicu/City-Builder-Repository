d18b2d051e1b835d1163aa655edcdf1bb4590147
Nicu <nicusor.laptedulce@gmail.com>
2021-06-14 23:07:57 +0300

    Added first scene

---
+++ CityBuilder/Assets/_CityBuilder/Scripts/UI/ChangeScene.cs
@@ -0,0 +1,24 @@
+﻿using DG.Tweening;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace _CityBuilder.Scripts.UI
+{
+    public class ChangeScene : MonoBehaviour
+    {
+        private void Start()
+        {
+            transform.DOScale(Vector3.one * 1.05f, 2f).SetLoops(-1, LoopType.Yoyo);
+            GetComponent<Button>().onClick.AddListener(() =>
+            {
+                SceneManager.LoadScene("GamePlay");
+            });
+        }
+
+        private void OnDestroy()
+        {
+            transform.DOKill();
+        }
+    }
+}
